e52b51248fa00169345ee65dc11e4a829c08841b
Sparsh Bhardwaj <Sparsh.bhardwaj.14.99@gmail.com>
2022-06-20 23:48:59 +0530

    First Commit

---
+++ netflix_build/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgmaxLv2XdxAmGyLPAIOetBxO9pjSU26w",
+  authDomain: "netflix-build-67db4.firebaseapp.com",
+  projectId: "netflix-build-67db4",
+  storageBucket: "netflix-build-67db4.appspot.com",
+  messagingSenderId: "168107964386",
+  appId: "1:168107964386:web:200b3c718f454b62faebbb",
+};
